ee61859b27a628453c33bf31b2db4c214d10ff9a
jackismissing <nicolas.j.garnier@gmail.com>
2013-08-14 13:58:37 +0200

    Added dreams template

--- dreamcatcher/views.py
+++ dreamcatcher/views.py
@@ -1,6 +1,25 @@
+# -*- coding: utf-8 -*-
 from dreamcatcher import dreamcatcher
+from flask import render_template
 
 @dreamcatcher.route('/')
 @dreamcatcher.route('/index')
 def index():
-	return "Hello World"
+	user = { 'nickname': 'Nicolas' }
+	return render_template("index.html", title="Home", user = user)
+
+@dreamcatcher.route('/dreams')
+def showDreams():
+	begin = 'Last night, I dreamed that'
+	dreams = [
+		{
+			'author' : { 'nickname': 'Jackismissing' },
+			'content' : '%s I was coding' % begin
+		},
+		{
+			'author' : { 'nickname': u'Chloé' },
+			'content' : '%s I was flying with a duck' % begin
+		}
+	]
+
+	return render_template("dreams.html", title = "Dreams", dreams = dreams)
